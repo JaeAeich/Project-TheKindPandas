45a63da3d2763c6c01bba444e81e0660695029c4
Jae Aeich <javedh@iitbhilai.ac.in>
2023-03-06 18:41:27 +0530

    added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Hero from "./components/Footer";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 function App() {
 	return (
 		<div className="App bg-gray-200 h-screen w-screen ">
 			<Navbar />
 			<Hero />
+			<Footer />
 		</div>
 	);
 }
