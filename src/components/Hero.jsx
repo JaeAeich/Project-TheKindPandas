44504941628b44a54ee1af79fbcb4e88d1c468a5
Jae Aeich <javedh@iitbhilai.ac.in>
2023-03-06 16:23:50 +0530

    nav style added

---
+++ src/components/Hero.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+function Hero(props) {
+	return <div className="hero"></div>;
+}
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,27 @@ import { AiFillGithub, AiFillPhone } from "react-icons/ai";
 
 function Navbar() {
 	return (
-		<div className="navbar">
-			<div className="nav">
-				<div className="brand-name">CaseCraft</div>
-				<div className="contanct">
-					<div className="gh">
-						<div className="logo">
+		<div className="navbar w-full p-4 flex mx-auto">
+			<div className="nav flex w-full justify-between md:max-w-[1020px] md:mx-auto">
+				<div className="brand-name text-3xl md:text-5xl text-sky-800 font-semibold">
+					CaseCraft
+				</div>
+				<div className="contacts flex space-x-4">
+					<div className="gh flex justify-center items-center space-x-2">
+						<div className="logo text-sky-800">
 							<AiFillGithub />
 						</div>
-						<div className="name">Github</div>
+						<div className="name hidden md:block text-sm text-sky-800">
+							Github
+						</div>
 					</div>
-					<div className="gh">
-						<div className="logo">
+					<div className="contact flex justify-center items-center space-x-2">
+						<div className="logo text-sky-800">
 							<AiFillPhone />
 						</div>
-						<div className="name">Contact</div>
+						<div className="name hidden md:block text-sm text-sky-800">
+							Contact
+						</div>
 					</div>
 				</div>
 			</div>
